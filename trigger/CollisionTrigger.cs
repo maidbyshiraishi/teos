eb10becbcf00ccbce8fb4af78f2e73ff5a57a7a8
SHIRAISHI <SHIRAISHI@SH76K>
2026-01-22 13:24:55 +0900

    kemolofでの変更を取り込み。CollisionTriggerはteosでは未使用

--- trigger/CollisionTrigger.cs
+++ trigger/CollisionTrigger.cs
@@ -9,7 +9,28 @@ namespace teos.trigger;
 public partial class CollisionTrigger : Area2D
 {
     [Export]
+    public bool AutoConnectEnter { get; set; } = true;
+
+    [Export]
+    public bool AutoConnectExit { get; set; } = true;
+
+    [Export]
     public Node Target { get; set; }
+
+    public override void _Ready()
+    {
+        if (AutoConnectEnter)
+        {
+            _ = Connect(Area2D.SignalName.AreaEntered, new(this, MethodName.ExecArea2D));
+            _ = Connect(Area2D.SignalName.BodyEntered, new(this, MethodName.Exec));
+        }
+
+        if (AutoConnectExit)
+        {
+            _ = Connect(Area2D.SignalName.AreaExited, new(this, MethodName.ExecExitArea2D));
+            _ = Connect(Area2D.SignalName.BodyExited, new(this, MethodName.ExecExit));
+        }
+    }
 
     public void Exec(Node2D node)
     {
