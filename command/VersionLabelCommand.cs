912f9c592f85aa12e785364898e319fcfc58afb6
SHIRAISHI <SHIRAISHI@SH76K>
2026-04-03 10:45:46 +0900

    Godotの更新時、プロジェクトファイルのアップグレードで参照が壊れる現象を回避するため、バージョン情報を表示するLabelノードの参照方法を追加した

--- command/VersionLabelCommand.cs
+++ command/VersionLabelCommand.cs
@@ -13,13 +13,22 @@ public partial class VersionLabelCommand : CommandRoot
     [Export]
     public Label VersionLabel { get; set; }
 
+    public override void _Ready()
+    {
+        base._Ready();
+
+        if (VersionLabel is null && GetParent() is Label label)
+        {
+            VersionLabel = label;
+            ExecCommand(null, true);
+        }
+    }
+
     public override void ExecCommand(Node node, bool flag)
     {
-        if (ExecFlag != flag || VersionLabel is null)
+        if (VersionLabel is not null)
         {
-            return;
+            VersionLabel.Text = ProjectSettings.GetSetting("application/config/version").AsString();
         }
-
-        VersionLabel.Text = ProjectSettings.GetSetting("application/config/version").AsString();
     }
 }
